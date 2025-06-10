082706b26321651281f26723eed932ef5621ddef
clopezbyte <clopez@buhoms.com>
2025-06-10 17:28:59 -0600

    iac

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,6 +113,7 @@ resource "google_cloud_run_v2_job" "in-out-analytics-pipeline" {
   }
 }
 
+# Cloud Run Job for dbt transformation from Bronze to Silver
 resource "google_cloud_run_v2_job" "in-out-analytics-dbt-job" {
   name = "in-out-analytics-dbt-job"
   location = var.region
@@ -122,7 +123,7 @@ resource "google_cloud_run_v2_job" "in-out-analytics-dbt-job" {
   template {
     template {
       containers {
-        image = "gcr.io/b-materials/in-out-analytics-dbt-job:dev"  #Docker image
+        image = "gcr.io/b-materials/in-out-analytics-dbt-job:prodv1"  #Docker image
 
         env {
           name  = "DBT_KEY_URL"
@@ -131,4 +132,83 @@ resource "google_cloud_run_v2_job" "in-out-analytics-dbt-job" {
       }
     }
   }
+}
+
+#Cloud Composer
+provider "google-beta" {
+  alias   = "composer"
+  project = var.project_id
+  region  = var.region
+}
+
+# Cloud Composer Environment
+resource "google_composer_environment" "buho_claw_environment" {
+  provider = google-beta.composer
+  name     = "buho-claw-environment"
+  region   = var.region
+
+  labels = {
+    environment = "production"
+    team        = "dmca"
+    project     = var.project_id
+  }
+
+  config {
+    software_config {
+      image_version = "composer-3-airflow-2.10.5-build.3"
+
+      airflow_config_overrides = {
+        core-dag_serialization = "True"
+        core-default_timezone  = "America/Mexico_City"
+      }
+    }
+
+    node_config {
+      service_account = var.composer_service_account
+    }
+
+    resilience_mode = "STANDARD_RESILIENCE"
+    environment_size = "ENVIRONMENT_SIZE_SMALL"
+
+    maintenance_window {
+      start_time = "2023-01-01T00:00:00Z"
+      end_time   = "2023-01-01T07:00:00Z"
+      recurrence = "FREQ=WEEKLY;BYDAY=SA,SU"
+    }
+
+    database_config {
+      zone = "us-central1-a"  
+    }
+
+    recovery_config {
+      scheduled_snapshots_config {
+        enabled = false
+      }
+    }
+
+    #Network config
+    # network_config {
+    #   network                    = var.network_id
+    #   subnetwork                 = var.subnet_id
+    #   enable_private_environment = true
+    #   master_ipv4_cidr_block     = "10.0.0.0/28"
+    #   worker_ipv4_cidr_block     = "10.0.1.0/24"
+    # }
+
+    #Dataplex Lineage config
+
+    web_server_network_access_control {
+      allowed_ip_range {
+        value       = "0.0.0.0/0"
+        description = "Allow access from all IP addresses"
+      }
+    }
+  }
+}
+
+# In Out Analytics Pipeline DAG for Cloud Composer
+resource "google_storage_bucket_object" "in_out_analytics_pipeline_dag" {
+  name   = "dags/in_out_analytics_pipeline_dag.py"  
+  bucket = var.composer_dag_bucket  
+  source = "C:/Users/Carlos Lopez/app-entradas-salidas/analytics/dags/in_out_analytics_pipeline_dag.py"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,15 @@ variable "keys_gcs_bucket" {
     description = "Google Cloud Storage Bucket for keys"
     default = "b-materials-keys"
 }
+
+variable "composer_service_account" {
+  type        = string
+  description = "Name of the Cloud Composer service account"
+  default     = "cloud-run-invoker@b-materials.iam.gserviceaccount.com"
+}
+
+variable "composer_dag_bucket" {
+  type = string
+  description = "Cloud Composer DAG bucket storage"
+  default = "us-central1-buho-claw-envir-ca3ff9c9-bucket"
+}
